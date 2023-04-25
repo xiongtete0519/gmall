9a82d29b700289d319f12e73a7a51fd7e7d28cb9
CodeLeader <1144722582@qq.com>
2023-04-25 22:43:52 +0800

    异步编排优化校验

--- service/service-order/src/main/java/com/atguigu/gmall/order/controller/OrderApiController.java
+++ service/service-order/src/main/java/com/atguigu/gmall/order/controller/OrderApiController.java
@@ -12,6 +12,7 @@ import com.atguigu.gmall.order.service.OrderService;
 import com.atguigu.gmall.product.client.ProductFeignClient;
 import com.atguigu.gmall.user.client.UserFeignClient;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -20,9 +21,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.stream.Collectors;
 
 @RestController
@@ -45,6 +46,9 @@ public class OrderApiController {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @Autowired
+    private ThreadPoolExecutor executor;
+
     @ApiOperation("提交订单")
     @PostMapping("/auth/submitOrder")
     public Result submitOrder(@RequestBody OrderInfo orderInfo,HttpServletRequest request){
@@ -59,32 +63,55 @@ public class OrderApiController {
         }
         //验证库存
         List<OrderDetail> orderDetailList = orderInfo.getOrderDetailList();
+
+        //定义集合收集异步对象
+        List<CompletableFuture> completableFutureList=new ArrayList<>();
+
+        //定义集合收集错误信息
+        List<String> errorList=new ArrayList<>();
         if(!CollectionUtils.isEmpty(orderDetailList)){
             for (OrderDetail orderDetail : orderDetailList) {
-                //验证库存
-                boolean flag = orderService.checkStock(String.valueOf(orderDetail.getSkuId()),
-                        String.valueOf(orderDetail.getSkuNum()));
-                //处理
-                if(!flag){
-                    return Result.fail().message(orderDetail.getSkuId()+orderDetail.getSkuName()+"库存不足！");
-                }
+                //异步编排--多线程操作
+                CompletableFuture<Void> stockCompletableFuture = CompletableFuture.runAsync(() -> {
+                    //验证库存
+                    boolean flag = orderService.checkStock(String.valueOf(orderDetail.getSkuId()),
+                            String.valueOf(orderDetail.getSkuNum()));
+                    //处理
+                    if (!flag) {
+//                        return Result.fail().message(orderDetail.getSkuId()+orderDetail.getSkuName()+"库存不足！");
+                         errorList.add(orderDetail.getSkuId()+orderDetail.getSkuName()+"库存不足！");
+                    }
+                }, executor);
+                completableFutureList.add(stockCompletableFuture);
+
+
                 //校验价格 缓存和mysql
-                //获取实时价格
-                BigDecimal skuPrice = productFeignClient.getSkuPrice(orderDetail.getSkuId());
-                //比较
-                if(orderDetail.getOrderPrice().compareTo(skuPrice)!=0){
-                    //设置新的内容
-                    List<CartInfo> cartCheckedList = cartFeignClient.getCartCheckedList(userId);
-                    //更新到缓存
-                    cartCheckedList.forEach(CartInfo->{
-                        BoundHashOperations<String,String,CartInfo> boundHashOps = redisTemplate.boundHashOps(RedisConst.USER_KEY_PREFIX + userId + RedisConst.USER_CART_KEY_SUFFIX);
-                        boundHashOps.put(CartInfo.getSkuId().toString(),CartInfo);
-                    });
+                CompletableFuture<Void> skuPriceCompletableFuture = CompletableFuture.runAsync(() -> {
+                    //获取实时价格
+                    BigDecimal skuPrice = productFeignClient.getSkuPrice(orderDetail.getSkuId());
+                    //比较
+                    if (orderDetail.getOrderPrice().compareTo(skuPrice) != 0) {
+                        //设置新的内容
+                        List<CartInfo> cartCheckedList = cartFeignClient.getCartCheckedList(userId);
+                        //更新到缓存
+                        cartCheckedList.forEach(CartInfo -> {
+                            BoundHashOperations<String, String, CartInfo> boundHashOps = redisTemplate.boundHashOps(RedisConst.USER_KEY_PREFIX + userId + RedisConst.USER_CART_KEY_SUFFIX);
+                            boundHashOps.put(CartInfo.getSkuId().toString(), CartInfo);
+                        });
 
-                    return Result.fail().message(orderDetail.getSkuId()+"价格有变动");
-                }
-
+//                        return Result.fail().message(orderDetail.getSkuId()+"价格有变动");
+                        errorList.add(orderDetail.getSkuId()+"价格有变动");
+                    }
+                }, executor);
+                completableFutureList.add(skuPriceCompletableFuture);
             }
+        }
+        //声明为一组
+        CompletableFuture.allOf(completableFutureList.toArray(new CompletableFuture[completableFutureList.size()]))
+                .join();
+        //错误集合信息决定是否下订单
+        if(errorList.size()>0){
+            return Result.fail().message(StringUtils.join(errorList,","));
         }
 
         orderInfo.setUserId(Long.parseLong(userId));
