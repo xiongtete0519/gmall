880c545fcbba107e4b259bb671c3fee7a61c9966
CodeLeader <1144722582@qq.com>
2023-05-14 15:38:59 +0800

    支付宝支付-库存扣减成功后消费消息

--- service/service-order/src/main/java/com/atguigu/gmall/order/receiver/OrderReceiver.java
+++ service/service-order/src/main/java/com/atguigu/gmall/order/receiver/OrderReceiver.java
@@ -1,5 +1,6 @@
 package com.atguigu.gmall.order.receiver;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.gmall.constant.MqConst;
 import com.atguigu.gmall.model.enums.OrderStatus;
 import com.atguigu.gmall.model.enums.ProcessStatus;
@@ -14,6 +15,9 @@ import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
 
 @Component
 public class OrderReceiver {
@@ -86,6 +90,41 @@ public class OrderReceiver {
             e.printStackTrace();
         }
 
+
+        //手动确认
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+    }
+
+    //库存扣减成功，更新操作
+    @SneakyThrows
+    @RabbitListener(bindings = @QueueBinding(
+            value =@Queue(value = MqConst.QUEUE_WARE_ORDER,durable = "true",autoDelete = "false"),
+            exchange =@Exchange(value =MqConst.EXCHANGE_DIRECT_WARE_ORDER,autoDelete = "false"),
+            key ={MqConst.ROUTING_WARE_ORDER}
+    ))
+    public void stockOrderStatus(String strJSON,Message message,Channel channel){
+
+        try {
+            //判断
+            if(!StringUtils.isEmpty(strJSON)){
+                //转换数据类型
+                Map<String,String> map = JSON.parseObject(strJSON, Map.class);
+                //获取订单id
+                String orderId = map.get("orderId");
+                //获取状态
+                String status = map.get("status");
+                //判断是否扣减成功
+                if("DEDUCTED".equals(status)){
+                    orderService.updateOrderStatus(Long.parseLong(orderId),ProcessStatus.WAITING_DELEVER);
+                }else{
+                    orderService.updateOrderStatus(Long.parseLong(orderId),ProcessStatus.STOCK_EXCEPTION);
+                    //通知管理员，人工客服，商家
+                }
+            }
+        } catch (NumberFormatException e) {
+            //出现异常，邮件、短信等通知
+            e.printStackTrace();
+        }
 
         //手动确认
         channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
